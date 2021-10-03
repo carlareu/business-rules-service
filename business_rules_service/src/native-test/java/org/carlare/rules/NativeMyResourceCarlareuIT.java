7c9da2758c88a204b09fdc033abb23b079616bf4
carlareu <carlos@tymit.com>
2021-10-03 21:37:43 +0200

    first commit with quarkus project

---
+++ business_rules_service/src/native-test/java/org/carlare/rules/NativeMyResourceCarlareuIT.java
@@ -0,0 +1,9 @@
+package org.carlare.rules;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMyResourceCarlareuIT extends MyResourceCarlareuTest {
+
+    // Execute the same tests but in native mode.
+}
