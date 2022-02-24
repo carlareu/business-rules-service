1748c950a335a80bfefb9b60a1968dbdfcf8e056
Carlos Areu <carlos@tymit.com>
2022-02-24 12:56:09 +0100

    feature: skeleton fix branches

---
+++ src/native-test/java/com/tymit/rules/NativeMyResourceCarlareuIT.java
@@ -0,0 +1,9 @@
+package com.tymit.rules;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMyResourceCarlareuIT extends MyResourceCarlareuTest {
+
+    // Execute the same tests but in native mode.
+}
