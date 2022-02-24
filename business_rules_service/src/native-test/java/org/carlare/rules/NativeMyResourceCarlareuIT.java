30d0bd9741b643fe7348c0446c4ec435521e4818
Carlos Areu <carlos@tymit.com>
2022-02-24 12:44:32 +0100

    feature: first skeleton

--- business_rules_service/src/native-test/java/org/carlare/rules/NativeMyResourceCarlareuIT.java
+++
@@ -1,9 +0,0 @@
-package org.carlare.rules;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeMyResourceCarlareuIT extends MyResourceCarlareuTest {
-
-    // Execute the same tests but in native mode.
-}
